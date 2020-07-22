c1cfc64880382def5afb7fb143ca3591ab6035a2
sklakshmana <sklakshmana@gmail.com>
2020-07-22 22:15:56 +0530

    Update the files

--- Framework/src/test/java/com/learnautomation/pages/BaseClass.java
+++ Framework/src/test/java/com/learnautomation/pages/BaseClass.java
@@ -10,6 +10,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -43,12 +44,18 @@ public class BaseClass {
 		Reporter.log("Setting Done- Test can be started", true);		
 	}
 	
-	
+	@Parameters({"browser","urlTobeTested"})
 	@BeforeClass
-	public void setup()
+	public void setup(String browser,String url)
 	{
+		System.out.println("browser "+browser+" URL "+url);
 		Reporter.log("Trying to start Browser and Getting application ready", true);
-		driver=BrowserFactory.startApplication(driver, config.getBrowser(),config.getStagingURL() );
+		
+		//driver=BrowserFactory.startApplication(driver, config.getBrowser(),config.getStagingURL() );
+		
+		System.out.println(url);
+		
+		driver=BrowserFactory.startApplication(driver,browser,url);
 		
 		Reporter.log("Browser and Application is up and running", true);
 	}
